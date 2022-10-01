3148cb92d4eafe584f3a00551ed136dcbfdb20cc
Andrew Patterson <patto@live.com.au>
2022-10-01 01:40:48 +0100

    Implementation of 'PHPUnit: Run test method' command

--- src/runner/CommandHandler.ts
+++ src/runner/CommandHandler.ts
@@ -30,19 +30,17 @@ export class CommandHandler {
     }
 
     public async execute(command: string) {
+        const editor = vscode.window.activeTextEditor;
+        let testItem: vscode.TestItem | undefined;
         let includes: vscode.TestItem[];
         let request: vscode.TestRunRequest;
         let cancellationTokenSource = new vscode.CancellationTokenSource();
 
         switch (command) {
             case 'run.method':
-
-                break;
-            case 'run.class':
-                this.logger.info(`Running command: Run test class...`);
+                this.logger.info(`Running command: Run test method...`);
 
                 // Identify the file open in the active editor
-                const editor = vscode.window.activeTextEditor;
                 if (!editor) {
                     this.logger.warn(`No active editor found - cannot identify class to run!`);
                     return;
@@ -53,7 +51,45 @@ export class CommandHandler {
                 }
 
                 // Find test item definition for the document
-                let testItem = this.itemMap.getTestItemForClass(editor.document.uri);
+                let classTestItem = this.itemMap.getTestItemForClass(editor.document.uri);
+                if (!classTestItem) {
+                    this.logger.warn(`No test item definition was found for the current class. Aborting test run.`);
+                    return;
+                }
+
+                // Find the closest method name above the location of the cursor
+                let currentLine = editor.selection.active.line;
+                for (let [id, methodTestItem] of classTestItem.children) {
+                    if (methodTestItem.range && methodTestItem.range.contains(editor.selection.active)) {
+                        testItem = methodTestItem;
+                    }
+                }
+                if (!testItem) {
+                    this.logger.warn(`No test item definition was found at the current cursor location. Aborting test run.`);
+                    return;
+                }
+
+                // Create test run request
+                includes = [ testItem ];
+                request = new vscode.TestRunRequest(includes);
+                await this.runner.run(request, cancellationTokenSource.token);
+                this.logger.info(`Command complete: Run test method`);
+                break;
+            case 'run.class':
+                this.logger.info(`Running command: Run test class...`);
+
+                // Identify the file open in the active editor
+                if (!editor) {
+                    this.logger.warn(`No active editor found - cannot identify class to run!`);
+                    return;
+                }
+                if (editor.document.languageId !== 'php') {
+                    this.logger.warn(`This command can only be executed on a PHPUnit test class (*.php file). If you have a PHPUnit test class open, make sure it is the active editor by clicking in it and then try again.`);
+                    return;
+                }
+
+                // Find test item definition for the document
+                testItem = this.itemMap.getTestItemForClass(editor.document.uri);
                 if (!testItem) {
                     this.logger.warn(`No test item definition was found for the current class. Aborting test run.`);
                     return;
