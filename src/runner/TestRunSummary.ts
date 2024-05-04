9c9b45b3ac4533d0e14135d754ebbfb0e9170a8e
Andrew Patterson <patto@live.com.au>
2024-05-05 00:41:16 +1000

    Shift peek for test failures to appear on the line where the test failure occurred Fixes #98

--- src/runner/TestRunSummary.ts
+++ src/runner/TestRunSummary.ts
@@ -65,6 +65,14 @@ export class TestRunSummary {
                 break;
         }
 
+        // If a test failure or error occurred, set the location of the message to the location in the test where the failure occurred
+        if (message) {
+            let testFileUri = result.getTestItem().uri;
+            let testFileLineNum = result.getMessageLineNum();
+            if (testFileUri && testFileLineNum) {
+                message.location = new vscode.Location(testFileUri, new vscode.Position((testFileLineNum - 1), 0));
+            }
+        }
         return message;
     }
 
